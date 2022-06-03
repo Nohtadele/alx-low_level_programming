0a8256c25992936baa092d58e18191042bdea382
Nohtadele <nohtad25@gmail.com>
2022-06-03 11:38:37 +0000

    last digit of number - last commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/* more headers goes there
+ * main - check the last digit of a number
+ * betty style doc for function main goes there */
 int main(void)
 {
 	int n;
@@ -12,7 +12,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n % 10 > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-	else if (n % 10 < 6 && n%10 > 0)
+	else if (n % 10 < 6 && n % 10 > 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	else if (n % 10 < 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
