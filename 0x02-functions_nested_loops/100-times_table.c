06bc206c5674543e9420c60392ed01f340104ecd
Nohtadele <nohtad25@gmail.com>
2022-06-07 16:26:26 +0000

    Times table of n up to 15

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,39 +1,42 @@
 #include "main.h"
 /**
- * times_table - function
- * Description: Print the 9 time matrix table
+ * print_times_table - function
+ * Description: Print the n time matrix table up to 15
  * Return: 0 success
  */
-void print_times_table(void)
+void print_times_table(int n)
 {
 	int i, j, mul;
 
-	for (i = 0 ; i < 10 ; i++)
+	if (n <= 15 && n >= 0)
 	{
-		for (j = 0 ; j < 10 ; j++)
+		for (i = 0 ; i <= n ; i++)
 		{
-			mul = i * j;
-			if ((mul / 10) == 0)
+			for (j = 0 ; j <= n ; j++)
 			{
+				mul = i * j;
 				if (j != 0)
 				{
-					_putchar(' ');
-					_putchar(' ');
+					if (mul <= 999)
+						_putchar(' ');
+					if (mul <= 99)
+						_putchar(' ');
+					if (mul <= 9)
+						_putchar(' ');
 				}
-				_putchar(mul + '0');
-				if (j != 9)
-					_putchar(',');
-			}
-			else
-			{
-				if (j != 0)
-					_putchar(' ');
-				_putchar((mul / 10) + '0');
+				if (mul >= 100)
+				{
+					_putchar((mul / 100) + '0');
+					_putchar(((mul / 10) % 10) + '0');
+				}
+				else
+					if (mul > 9 && mul < 100)
+						_putchar((mul / 10) + '0');
 				_putchar((mul % 10) + '0');
-				if (j != 9)
+				if (j != n)
 					_putchar(',');
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
