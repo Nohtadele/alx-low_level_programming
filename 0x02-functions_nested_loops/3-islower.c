149e3f8776a0deb6912c4d4e47de467a37bb2947
Nohtadele <nohtad25@gmail.com>
2022-06-07 10:54:56 +0000

    Check whether a character is lowecase or not

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,13 +3,14 @@
 /**
  * _islower - function
  * Checks whether a character is lowercase or not
+ * c - is the passed value of the character to check
  * Return: void - success
  */
 int _islower(int c)
 {
 	char ch;
-	ch = c;
 
+	ch = c;
 	if (ch >= 'a' && ch <= 'z')
 		return (1);
 	else
