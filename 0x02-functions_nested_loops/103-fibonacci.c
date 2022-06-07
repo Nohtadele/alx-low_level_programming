04dedd6c0b1dc0f18ef74d73a4f07fffc043f68d
Nohtadele <nohtad25@gmail.com>
2022-06-07 16:52:46 +0000

    sum of even-valued Fibonacci sequence up to 4000000

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,15 +3,17 @@
 
 /**
  * main - function
- * Description: Prints the sum of even-valued Fibonacci sequence no more than 4000000
+ * Description: Prints the sum of even-valued
+ * Fibonacci sequence no more than 4000000
  * Return: 0 - success
  */
 int main(void)
 {
+	int x = 0;
 	unsigned long f1 = 0, f2 = 1, sum;
 	float total;
 
-	while (1)
+	while (x == 0)
 	{
 		sum = f1 + f2;
 		if (sum > 4000000)
