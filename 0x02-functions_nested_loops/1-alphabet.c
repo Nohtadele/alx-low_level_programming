46105e24ffc4ff01e7410c4664c9f13859d04a50
Nohtadele <nohtad25@gmail.com>
2022-06-07 10:24:33 +0000

    Print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function
+ * Print lowercase alphabets
+ * Return: void - success
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+		_putchar(alpha);
+	_putchar('\n');
+}
