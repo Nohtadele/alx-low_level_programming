29fa3bcdb097f431cefe9a69fc40a80033e7f7a1
Nohtadele <nohtad25@gmail.com>
2022-06-13 20:11:31 +0000

    Reversing string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - function
+ * Description: Print string in reverse
+ * @s: pointer points to the string value
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int i, count;
+	char tmp;
+	
+	count = 0;
+	while (*(s + count) != '\0')
+		count++;
+	for (i = 0 ; i < count / 2 ; i++)
+	{
+		tmp = *(s + ((count - 1) - i));
+		*(s + ((count - 1) - i)) = *(s + i);
+		*(s + i) = tmp;
+	}
+}
