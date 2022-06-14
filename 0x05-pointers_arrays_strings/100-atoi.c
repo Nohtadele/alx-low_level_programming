cf158a5f25e2f48c815a49a131a3322fb456a777
Nohtadele <nohtad25@gmail.com>
2022-06-14 21:02:03 +0000

    extract number from string including sign

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - function
+ * Description: Extract integer with the preceeding sign
+ * from string value
+ * @s: pointer which points to a string value
+ * Return: Extracted integer or 0 if there is no integer
+ */
+int _atoi(char *s)
+{
+	unsigned int num;
+	int flag, neg_count, pos_count;
+
+	num = 0;
+	flag = 0;
+	neg_count = 0;
+	pos_count = 0;
+	while (*s)
+	{
+		if (*s >= '0' && *s <= '9')
+		{
+			num = num * 10 + *s - '0';
+			flag = 1;
+		}
+		else
+			if (flag == 1)
+				break;
+		if (*s == '-')
+			neg_count++;
+		else
+			if (*s == '+')
+				pos_count++;
+		s++;
+	}
+	if (flag == 1 && neg_count > pos_count)
+		num *= -1;
+	if (flag == 1)
+		return (num);
+	else
+		return (0);
+}
+
