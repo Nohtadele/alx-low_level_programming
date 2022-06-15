cf09a6600be58d075dfccdf6f151690e83262650
Nohtadele <nohtad25@gmail.com>
2022-06-15 18:00:19 +0000

    Fix the function limit issue

--- 0x06-pointers_arrays_strings/1111.c
+++ 0x06-pointers_arrays_strings/1111.c
@@ -9,7 +9,7 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int count1, count2, x, tmp;
+	int count1, count2, x, full_size, tmp;
 
 	count1 = 0;
 	count2 = 0;
@@ -21,10 +21,13 @@ char *_strncat(char *dest, char *src, int n)
 		tmp = n;
 	else
 		tmp = count2;
+	full_size = count1 + tmp;
 	for (x = 0 ; x < tmp ; x++)
 	{
 		*(dest + count1) = *(src + x);
 		count1++;
 	}
+	if (n < count2)
+		*(dest + full_size) = '\0';
 	return (dest);
 }
