e6e09c2dd034cd7887e402cef8a60a5b2b8758a5
Nohtadele <nohtad25@gmail.com>
2022-06-15 19:18:09 +0000

    Change lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,14 +3,14 @@
 /**
  * string_toupper - function
  * Description: Convert lowercase to uppercase
- * @str: String value to be uppercase
+ * @s: String value to be uppercase
  * Return: changed string
  */
 char string_toupper(char *s)
 {
 	int i = 0;
 
-	while (str[i])
+	while (s[i])
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
 			s[i] -= 32;
