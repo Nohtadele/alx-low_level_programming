ff5b9d6d39ae19abc7b7bfe47a6847407dd2974c
Nohtadele <nohtad25@gmail.com>
2022-06-15 18:18:15 +0000

    Fix description issue

--- 0x06-pointers_arrays_strings/1122.c
+++ 0x06-pointers_arrays_strings/1122.c
@@ -9,25 +9,15 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int count1, count2, x, full_size, tmp;
+	int count1, x;
 
 	count1 = 0;
-	count2 = 0;
 	while (*(dest + count1) != '\0')
 		count1++;
-	while (*(src + count2) != '\0')
-		count2++;
-	if (n < count2)
-		tmp = n;
-	else
-		tmp = count2;
-	full_size = count1 + tmp;
-	for (x = 0 ; x < tmp ; x++)
+	for (x = 0 ; *(src + x) && x < n ; x++)
 	{
 		*(dest + count1) = *(src + x);
 		count1++;
 	}
-	if (n < count2)
-		*(dest + full_size) = '\0';
 	return (dest);
 }
