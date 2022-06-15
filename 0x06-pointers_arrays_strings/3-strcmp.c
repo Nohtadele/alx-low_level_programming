aae51b6226bc0e45c020e20050fd0793ed3f17e4
Nohtadele <nohtad25@gmail.com>
2022-06-15 19:00:54 +0000

    copy string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - function
+ * Description: compare two strings
+ * @s1: pointer to the first string value
+ * @s2: pointer to the second string value
+ * @n: number of bytes to be copied
+ * Return: pointer value of the copied strings
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
