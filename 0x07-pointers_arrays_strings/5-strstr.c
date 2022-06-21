538294d031b0a1b88b439813b4722e195151c770
Nohtadele <nohtad25@gmail.com>
2022-06-21 15:51:21 +0000

    Search a substring inside a string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strstr - function
+ * Description: searches for a substring in a string
+ * @heystack: the given string
+ * @needle: substring to search for
+ * Return: address of the first offurance of substring in the string
+ */
+char *_strstr(char *heystack, char *needle)
+{
+	int i;
+
+	while (*heystack)
+	{
+		i = 0;
+		if (heystack[i] == needle[i])
+		{
+			for (i = 0 ; heystack[i] == needle[i] ; i++)
+			{
+			if (needle[i + 1] == '\0')
+				return (heystack);
+			}
+		}
+		heystack++;
+	}
+	return (NULL);
+}
