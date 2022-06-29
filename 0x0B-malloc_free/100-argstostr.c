60366cbada42679a6d04fd3acd7b6aed22ea9fd2
Nohtadele <nohtad25@gmail.com>
2022-06-29 18:26:19 +0000

    Merge strings from arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -4,32 +4,35 @@
 /**
  * argstostr - function
  * Description: Merge all the string arguments passed
- * @ac:
- * @av:
+ * @ac: Number of argumets passed
+ * @av: A pointer to the strings pointer collection
  * Return: a pointer to the new string or else NULL
  */
 char *argstostr(int ac, char **av)
 {
-	char *s;
-	int i, j;
+	char *s, *tmp;
+	int i, j, size;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-
-	for (i = 1 ; i < ac ; i++)
-	{
+	size = 0;
+	for (i = 0 ; i < ac ; i++)
 		for (j = 0 ; av[i][j] ; j++)
-		{
 			size++;
-		}
-	}
-	s = malloc(sizeof(char) * (size + ac));
-	if (s == NULL)
+	s = malloc(sizeof(char) * (size + ac + 1));
+	tmp = s;
+	if (tmp == NULL)
 		return (NULL);
-
 	for (i = 0 ; i < ac ; i++)
 	{
 		for (j = 0 ; av[i][j] ; j++)
 		{
-			s
-
+			*tmp = av[i][j];
+			tmp++;
+		}
+		*tmp = '\n';
+		tmp++;
+	}
+	*tmp = '\0';
+	return (s);
+}
