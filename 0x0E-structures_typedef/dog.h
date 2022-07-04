805bfbebc18c4e2a0f71e582ecdd809575d3c9ad
Nohtadele <nohtad25@gmail.com>
2022-07-04 20:54:06 +0000

    fix requirment on struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,9 +17,8 @@ struct dog
 /**
  * dog_t - Define for struct dog
  */
-
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
 
 #endif
