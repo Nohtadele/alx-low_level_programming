db7213a20745991324f4296b2067a1d5fe7d12e0
Nohtadele <nohtad25@gmail.com>
2022-07-04 20:07:00 +0000

    Dog info

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_INFO
+#define DOG_INFO
+
+/**
+ * struct dog - dog information
+ * @name: The name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Define for struct dog
+ */
+typedef struct dog dog_t;
+
+#endif
