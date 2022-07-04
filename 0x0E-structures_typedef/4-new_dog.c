1102928bf4e173071952091e7ebb0a747a16a657
Nohtadele <nohtad25@gmail.com>
2022-07-04 21:32:31 +0000

    fix isuue on copying string

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,7 +17,7 @@ char *_strcpy(char *dest, char *src)
 	int i, len;
 
 	len = _strlen(src);
-	for (i = 0 ; i <= len ; i++)
+	for (i = 0 ; i < len ; i++)
 		*dest++ = *src++;
 	*dest = '\0';
 	return (tmp);
