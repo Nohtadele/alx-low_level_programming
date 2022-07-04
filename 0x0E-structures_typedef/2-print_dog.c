e03a2273f303dacc24b77f654f46437b818759e6
Nohtadele <nohtad25@gmail.com>
2022-07-04 20:44:16 +0000

    Print dog information NULL fixed

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,5 +10,18 @@
  */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\nAge: %1f\nOwner: %s\n", d->name, d->age, d->owner);
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	if (d->age < 0)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %1f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
