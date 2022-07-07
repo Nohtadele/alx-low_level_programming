b9f519ea81170785eb1f83044c53ca25799a9d74
Nohtadele <nohtad25@gmail.com>
2022-07-07 07:37:55 +0000

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - function pointer
+ * Description: Prints the given name with UC or As it is
+ * @name: a pointer to the string of input
+ * @f: a function pointer to any of the functions it is called
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
