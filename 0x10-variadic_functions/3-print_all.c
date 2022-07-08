4ce9e03e1ee994c22f012afb7ea5c9735dba3d8e
Nohtadele <nohtad25@gmail.com>
2022-07-08 22:36:39 +0000

    Prints all data type passed at once

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,102 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+void print_char(va_list all_str);
+void print_int(va_list all_str);
+void print_fraction(va_list all_str);
+void print_str(va_list all_str);
+
+/**
+ * print_all - function
+ * Description: prints all the given string and numbers
+ * @separator: a string to separate two strings
+ * @format: ncharacter type with string value
+ * Return: Nothing
+ */
+void print_all(const char * const format, ...)
+{
+	int i, j;
+	va_list all_str;
+	func_list functions[] = {{"c", print_char},
+		{"i", print_int},
+		{"f", print_fraction},
+		{"s", print_str}};
+
+	va_start(all_str, format);
+	i = 0;
+	while (format && (*(format + i)))
+	{
+		j = 0;
+
+		while (j < 4 && (*(format + i) != *(functions[j].sym)))
+			j++;
+		if (j < 4)
+		{
+			functions[j].func_ptr(all_str);
+			if (j != 3)
+				printf(", ");
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(all_str);
+}
+
+/**
+ * print_char - prints a char data type
+ * @all_str: list of argumets passed, a pointer to the character to print
+ * Return: Nothing
+ */
+void print_char(va_list all_str)
+{
+	char ch;
+
+	ch = va_arg(all_str, int);
+	printf("%c", ch);
+}
+
+/**
+ * print_int - prints integer number
+ * @all_str: list of argumets passed, a pointer to the integer to print
+ * Return: Nothing
+ */
+void print_int(va_list all_str)
+{
+	int integer;
+
+	integer = va_arg(all_str, int);
+	printf("%d", integer);
+}
+
+/**
+ * print_fraction - prints fraction numbers
+ * @all_str: list of argumets passed, a pointer to the fraction to print
+ * Return: Nothing
+ */
+void print_fraction(va_list all_str)
+{
+	float frac;
+
+	frac = va_arg(all_str, double);
+	printf("%f", frac);
+}
+
+/**
+ * print_str - prints strings
+ * @all_str: list of argumets passed, a pointer to the string to print
+ * Return: Nothing
+ */
+void print_str(va_list all_str)
+{
+	char *str;
+
+	str = va_arg(all_str, char *);
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,23 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct printall - structure
+ * Description: contains the variables of the datatype symbol and
+ *		its corresponding function
+ * @sym: the specific character symbol for the corresponding function
+ * @func_ptr: the corresponding datatype function pointer
+ */
+typedef struct printall
+{
+	char *sym;
+	void (*func_ptr)(va_list all_str);
+} func_list;
 
 #endif
