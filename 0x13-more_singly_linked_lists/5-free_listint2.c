ecc5751daf27ca06d79144c958d8d65a31729f8b
Nohtadele <nohtad25@gmail.com>
2022-07-20 08:44:17 +0000

    Free memory allocated by linked list and set head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free memory of listint_t list, set head to NULL
+ * @head: pointer to the head of the listint_t list
+ * Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *free_tmp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		free_tmp = (*head)->next;
+		free(*head);
+		*head = free_tmp;
+	}
+	head == NULL;
+}
