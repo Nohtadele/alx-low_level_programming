a0bab46dc55308123914592ec9cb4703a46d0719
Nohtadele <nohtad25@gmail.com>
2022-07-22 10:46:23 +0000

    Binary to Decimal Convertion

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: Binary number, a pointer to a string of 0 and 1 chars
+ * Return: the converted unsigned int number or 0 if given input is NULL
+ *	or if there is a character in the given string other than 0 and 1
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int binr = 1, dec = 0;
+	int len = -1;
+	const char *temp = b;
+
+	if (b == NULL)
+		return (0);
+	while (*temp)
+	{
+		len++;
+		temp++;
+	}
+	while (len >= 0)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+		dec += (b[len] - '0') * binr;
+		binr *= 2;
+		len--;
+	}
+	return (dec);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,7 @@
+#ifndef BIT_MANIPULATION
+#define BIT_MANIPULATION
+
+int _putchar(char ch);
+unsigned int binary_to_uint(const char *b);
+
+#endif
