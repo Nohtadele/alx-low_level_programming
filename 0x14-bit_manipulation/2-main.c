ff2d14bdaf638e5c61d24abdfa20f18c54c1c8f7
Nohtadele <nohtad25@gmail.com>
2022-07-25 10:05:01 +0000

    value of a bit at a given index

--- 0x14-bit_manipulation/2-main.c
+++ 0x14-bit_manipulation/2-main.c
@@ -16,9 +16,18 @@ int main(void)
 	printf("%d\n", n);
 	n = get_bit(1024, 0);
 	printf("%d\n", n);
-	n = get_bit(9, 4);
-	printf("%d\n", n);
 	n = get_bit(9, 3);
 	printf("%d\n", n);
+	n = get_bit(9, 4);
+	printf("%d\n", n);
+	n = get_bit(9, 5);
+	printf("%d\n", n);
+	n = get_bit(9, 6);
+	printf("%d\n", n);
+	n = get_bit(9, 7);
+	printf("%d\n", n);
+	n = get_bit(9, 8);
+	printf("%d\n", n);
+	n = get_bit(9, 9);
 	return (0);
 }
