c81a96e8d17839eafdff870cdf65fda7bcbe1067
Nohtadele <nohtad25@gmail.com>
2022-07-25 10:47:04 +0000

    fix error on flip bit to 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,14 +8,8 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int bi;
-
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	bi = *n >> index;
-	if ((bi & 1) == 0)
-		*n &= (1 << index);
-	else
-		*n ^= (1 << index);
+	*n &= ~(1 << index);
 	return (1);
 }
